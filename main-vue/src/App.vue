1d3267dc4b30a26280ea85bfe83bb2f8836dde4d
mk965 <mencre@163.com>
2021-07-30 23:22:41 +0800

    :sparkles: feat: add main-vue

---
+++ main-vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+    <div id="subapp-viewport"></div>
+  </div>
+</template>
+
+<script>
+import { registerMicroApps, start } from 'qiankun';
+import microApps from './micro-app';
+
+export default {
+  mounted () {
+    registerMicroApps(microApps);
+    start();
+  }
+}
+</script>
+
+
+<style lang="scss">
+</style>
---
+++ main-vue/src/micro-app.js
@@ -0,0 +1,31 @@
+const microApps = [
+  {
+    name: 'sub-react',
+    entry: '//localhost:9001/',
+    activeRule: '/sub-react',
+    container: '#subapp-viewport', // 子应用挂载的div
+    props: {
+      routerBase: '/sub-react'
+    }
+  },
+  {
+    name: 'sub-vue',
+    entry: '//localhost:9002/',
+    activeRule: '/sub-vue',
+    container: '#subapp-viewport', // 子应用挂载的div
+    props: {
+      routerBase: '/sub-vue'
+    }
+  },
+  {
+    name: 'sub-vue3',
+    entry: '//localhost:9003/',
+    activeRule: '/sub-vue3',
+    container: '#subapp-viewport', // 子应用挂载的div
+    props: {
+      routerBase: '/sub-vue3'
+    }
+  }
+]
+
+export default microApps;
